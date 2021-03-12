3869ad19ab8856074ccc9f52862d34e90b1f7093
Rinta <Rinta@DESKTOP-EEA9BTQ>
2021-03-12 15:28:50 +0000

    Improved comments in CommandHandler and Program

--- Services/CommandHandler.cs
+++ Services/CommandHandler.cs
@@ -1,95 +1,99 @@
-﻿using System;
+﻿// Not being used at the moment
+
+using System;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration; // Presumably not available for .Net Framework 4.7.2, might have to install .Net Core later
 
 namespace BirthdayBot.Services
 {
     public class CommandHandler
     {
-        // setup fields to be set later in the constructor
+        // Fields to be set later in the constructor
         private readonly IConfiguration _config;
         private readonly CommandService _commands;
         private readonly DiscordSocketClient _client;
         private readonly IServiceProvider _services;
 
-        public CommandHandler(IServiceProvider services)
+        public CommandHandler(IServiceProvider services) // This constructor accepts its dependency as an input, which is called "Dependency Injection"
         {
-            // juice up the fields with these services
-            // since we passed the services in, we can use GetRequiredService to pass them into the fields set earlier
+            // Since we passed the services in, we can use GetRequiredService to pass them into the fields set earlier
+            // It still expects DiscordSocketClient, so it is not abstract -- it merely splits out the implementation from the call
             _config = services.GetRequiredService<IConfiguration>();
             _commands = services.GetRequiredService<CommandService>();
             _client = services.GetRequiredService<DiscordSocketClient>();
             _services = services;
 
-            // take action when we execute a command
+            // Take action when we execute a command
             _commands.CommandExecuted += CommandExecutedAsync;
 
-            // take action when we receive a message (so we can process it, and see if it is a valid command)
+            // Take action when we receive a message (so we can process it, and see if it is a valid command)
             _client.MessageReceived += MessageReceivedAsync;
         }
 
-        public async Task InitializeAsync()
+        public async Task InitializeAsync() // Commands are modular (i.e. plugins) and this method loads them up
         {
-            // register modules that are public and inherit ModuleBase<T>.
+            // Registers modules that are public and inherit ModuleBase<T>
             await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
         }
 
-        // this class is where the magic starts, and takes actions upon receiving messages
+        // Takes actions upon receiving messages
         public async Task MessageReceivedAsync(SocketMessage rawMessage)
         {
-            // ensures we don't process system/other bot messages
+            // Ensures we don't process system messages / messages from other bots received via Web
             if (!(rawMessage is SocketUserMessage message))
             {
                 return;
             }
 
+            // Ensures we don't process system messages / messages from other bots received via other means
             if (message.Source != MessageSource.User)
             {
                 return;
             }
 
-            // sets the argument position away from the prefix we set
+            // Sets the argument position away from the prefix we set
             var argPos = 0;
 
-            // get prefix from the configuration file
+            // Gets prefix from the configuration file
             char prefix = Char.Parse(_config["Prefix"]);
 
-            // determine if the message has a valid prefix, and adjust argPos based on prefix
+            // Determines if the message starts with @mention of the Bot OR has a valid prefix, and adjusts argPos accordingly; exits if neither is true
             if (!(message.HasMentionPrefix(_client.CurrentUser, ref argPos) || message.HasCharPrefix(prefix, ref argPos)))
             {
                 return;
             }
-
+            
+            // If we are still here - creates context of the received message
             var context = new SocketCommandContext(_client, message);
 
-            // execute command if one is found that matches
+            // Executes command if one is found that matches message context
+            // If multiple commands are found - Exception will be thrown; This behavior can be changed via adding another arguement
+            // Presumably Exceptions are handled in CommandHandler.CommandExecutedAsync
             await _commands.ExecuteAsync(context, argPos, _services);
         }
 
         public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
         {
-            // if a command isn't found, log that info to console and exit this method
+            // If a command isn't found, log that info to console and exit this method
             if (!command.IsSpecified)
             {
                 System.Console.WriteLine($"Command failed to execute for [{context.User.Username}] <-> [{result.ErrorReason}]!");
                 return;
             }
 
-
-            // log success to the console and exit this method
+            // Log success to the console and exit this method
             if (result.IsSuccess)
             {
                 System.Console.WriteLine($"Command [{command.Value.Name}] executed for -> [{context.User.Username}]");
                 return;
             }
 
-
-            // failure scenario, let's let the user know
+            // Failure scenario, let the user know
             await context.Channel.SendMessageAsync($"Sorry, {context.User.Username}... something went wrong -> [{result}]!");
         }
     }
