7fcea62249061e25dcf66e20a2505b2e1b5f6e4a
Rinta <Rinta@DESKTOP-EEA9BTQ>
2021-03-15 17:58:16 +0000

    Added commands implementation via Dependency Injection

---
+++ Modules/BasicModule.cs
@@ -0,0 +1,18 @@
+﻿// .NET Base
+using System.Threading.Tasks;
+// Discord
+using Discord;
+using Discord.Commands;
+// Internal
+using BirthdayBot.Services;
+
+namespace BirthdayBot.BasicModule
+{
+    public class BasicModule : ModuleBase<SocketCommandContext>
+    { 
+        [Command("beep")]
+        [Alias("boop")]
+        public Task PingAsync()
+            => ReplyAsync("boop");
+        }
+}
