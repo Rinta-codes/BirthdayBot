37c986b48b52562479e4d63efec5e5c3ec30abc7
Rinta <55794209+Rinta-codes@users.noreply.github.com>
2021-03-16 18:03:42 +0000

    Implemented command for role change from @mention, picking up Role Name from config file

---
+++ TypeReaders/ConfigurationTypeReader.cs
@@ -0,0 +1,30 @@
+﻿// .NET Base
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+// Discord
+using Discord;
+using Discord.Commands;
+
+namespace BirthdayBot.TypeReaders
+{
+    class ConfigurationTypeReader : TypeReader
+    {
+        public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        {
+            IConfiguration config;
+            try
+            {
+                config = services.GetRequiredService<IConfiguration>();
+            }
+            catch (Exception e)
+            {
+                return Task.FromResult(TypeReaderResult.FromError(e));
+            }
+
+            if (config is null) return Task.FromResult(TypeReaderResult.FromError(CommandError.ObjectNotFound, "Configuration file not found."));
+            else return Task.FromResult(TypeReaderResult.FromSuccess(services.GetRequiredService<IConfiguration>()));
+        }
+    }
+}
