4d951a781fe071bc998c11460399179c62a0eb3f
Rinta <Rinta@DESKTOP-EEA9BTQ>
2021-03-17 20:33:41 +0000

    Updated logging to tag in source of log message Removed redundant TypeReaders Added guildusers command that retrieves full list of Server users via REST

--- Modules/BasicModule.cs
+++ Modules/BasicModule.cs
@@ -1,5 +1,7 @@
 ﻿// .NET Base
+using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Discord.Rest;
 // Internal
 using BirthdayBot.Services;
 
@@ -15,33 +18,67 @@ namespace BirthdayBot.BasicModule
     public class BasicModule : ModuleBase<SocketCommandContext>
     {
         private readonly IConfiguration _config;
-        public BasicModule(IConfiguration config)
+        private readonly DiscordRestClient _clientRest;
+        public BasicModule(IConfiguration config, DiscordRestClient clientRest)
         {
             _config = config;
+            _clientRest = clientRest;
         }
 
+        [Command("test")]
+        public async Task TestAsync(int x) => await ReplyAsync(x.ToString());
 
-        [Command("beep")]
+        [Command("beep")] // simple interaction
         [Alias("boop")]
         public async Task PingAsync()
             => await ReplyAsync("boop");
 
-        [Command("good bot")]
+        [Command("good bot")] // be polite to your bot
         public async Task GoodBotAsync()
             => await ReplyAsync("thank you");
 
-        [Command("birthdayme")]
+        [Command("birthdayme")] // Assign hardcoded role to command caller
         public async Task AssignBirthdayAsync()
         {
             // Resolve<BusSettings>()
             await (Context.User as SocketGuildUser).AddRoleAsync(Context.Guild.Roles.First(sp_role => sp_role.Name == "Birthday Cake"));
         }
 
-        [Command("birthday")]
+        [Command("birthday")] // Assign role to @mentioned user by Role Name from configuration
         public async Task AssignBirthdayAsync(SocketGuildUser user)
         {
             var roleName = _config["Role Name"];
             await user.AddRoleAsync(user.Guild.Roles.First(sp_role => sp_role.Name == roleName));
+        }
+
+        [Command("guildusers")] // Retrieve a full list of server users
+        public async Task GetGuildUsersAsync()
+        {
+            string response = "";
+
+            // Implementation via Guild.Users
+            // foreach (var x in Context.Guild.Users) { response += (x.Username + " "); };
+
+            //Implementation via REST Client directly
+            //RestGuild guildRest = await _clientRest.GetGuildAsync(Context.Guild.Id);
+            //await guildRest.GetUsersAsync().ForEachAsync(x =>
+            //{
+            //    foreach (IGuildUser y in x) { Console.Write("{name} ", y.Username); };
+            //});
+
+            // Implementation via Websocket method that uses REST API
+            await Context.Guild.GetUsersAsync().ForEachAsync(x =>
+            {
+                foreach (IGuildUser y in x) { response += y.Username + " "; };
+            });
+
+            // Sending response via REST for test purposes
+            RestGuild guildRest = await _clientRest.GetGuildAsync(Context.Guild.Id);
+            RestTextChannel channelRest = await guildRest.GetTextChannelAsync(Context.Channel.Id);
+            await channelRest.SendMessageAsync(response);
+
+            // Sending response via WebSocket
+            // await ReplyAsync(response);
         }
     }
 }
--- TypeReaders/ConfigurationTypeReader.cs
+++ TypeReaders/ConfigurationTypeReader.cs
@@ -5,26 +5,39 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 // Discord
 using Discord;
+using Discord.WebSocket;
+using Discord.Rest;
 using Discord.Commands;
 
 namespace BirthdayBot.TypeReaders
 {
-    class ConfigurationTypeReader : TypeReader
+    // Currently not working due to bug https://github.com/discord-net/Discord.Net/issues/1485
+    /*
+    class GuildUserTypeReader : TypeReader
     {
         public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
         {
-            IConfiguration config;
+            return Task.FromResult(TypeReaderResult.FromError(CommandError.Exception, "Just checking if this actually works..."));
+            SocketGuildUser guildUser;
             try
             {
-                config = services.GetRequiredService<IConfiguration>();
+                guildUser = services.GetRequiredService<DiscordRestClient>().GetGuildUserAsync(context.Guild.Id, context.User.Id) as IGuildUser as SocketGuildUser;
+                return Task.FromResult(TypeReaderResult.FromSuccess(guildUser));
             }
             catch (Exception e)
             {
                 return Task.FromResult(TypeReaderResult.FromError(e));
             }
+        }
+    }
+    */
 
-            if (config is null) return Task.FromResult(TypeReaderResult.FromError(CommandError.ObjectNotFound, "Configuration file not found."));
-            else return Task.FromResult(TypeReaderResult.FromSuccess(services.GetRequiredService<IConfiguration>()));
+    class TestTypeReader : TypeReader
+    {
+        public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        {
+            return Task.FromResult(TypeReaderResult.FromSuccess(5));
         }
     }
 }
+
