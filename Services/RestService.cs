667940f00729dfefbdad36da7e06169588650d0f
Rinta <55794209+Rinta-codes@users.noreply.github.com>
2021-03-26 16:32:25 +0000

    Added Dispose() for RestService; Minor edits

--- Services/RestService.cs
+++ Services/RestService.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace BirthdayBot.Services
 {
-    public class RestService
+    public class RestService : IDisposable
     {
-        private readonly HttpClient _client = new();
+        private readonly HttpClient _client = new(); // Might re-implement via HttpClientFactory
         private readonly IConfiguration _config;
         private readonly string _uriBase = "https://discord.com/api";
 
@@ -23,7 +23,8 @@ namespace BirthdayBot.Services
             _client.DefaultRequestHeaders.Clear();
             // Add Authentication token from configuration
             _client.DefaultRequestHeaders.Add("Authorization", "Bot" + " " + _config["Token"]);
-            // _client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            // _client.DefaultRequestHeaders.Add("Content-Type", "application/json"); // Not a request header, and not needed for add role call
+                                                                                      // will sort out later to be more universal
         }
 
         public async Task PutAsync(string requestString, HttpContent content)
@@ -31,12 +32,17 @@ namespace BirthdayBot.Services
             try
             {
                 var response = await _client.PutAsync(_uriBase + requestString, content);
-                if (!(response is null)) Console.WriteLine("[{0} {1}]", this.GetType().Assembly, response.Content.ToString());
+                if (!(response is null)) Console.WriteLine("[{0}] {1}", this.GetType().Assembly, response.Content.ToString());
             }
             catch (Exception e)
             {
-                Console.WriteLine("[{0} {1}]", this.GetType().Assembly, e.Message);
+                Console.WriteLine("[{0}] {1}", this.GetType().Assembly, e.Message);
             }
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
         }
     }
 }
