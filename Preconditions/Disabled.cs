2ad1287b0f0032e97d68b7caba922fd8f41aab18
Rinta <55794209+Rinta-codes@users.noreply.github.com>
2021-04-07 17:47:36 +0100

    Added a precondition to disable a command for everyone

---
+++ Preconditions/Disabled.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+public class Disabled : PreconditionAttribute
+{
+    public Disabled() {}
+
+    public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+    {
+        return Task.FromResult(PreconditionResult.FromError("This command is currently disabled."));
+    }
+}
