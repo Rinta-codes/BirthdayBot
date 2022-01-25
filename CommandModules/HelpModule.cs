148c6bdd92794eef100d656cbfb007435e403369
Rinta <55794209+Rinta-codes@users.noreply.github.com>
2022-01-25 10:05:09 +0000

    Optimise help command

--- CommandModules/HelpModule.cs
+++ CommandModules/HelpModule.cs
@@ -28,26 +28,26 @@ namespace BirthdayBot.CommandModules
                 .WithTitle("__Available Commands__")
                 .WithColor(Color.Magenta);
 
-
-            bool disabled = false;
             foreach (var command in _commands.Commands)
             {
-                foreach (var precondition in command.Preconditions)
-                    if (precondition is Disabled)
-                        disabled = true;
-
-                if (!disabled)
+                if (command.CheckPreconditionsAsync(Context).Result.IsSuccess)
                 {
-                    string aliases = new(string.Empty);
-                    foreach (var alias in command.Aliases)
-                        aliases += alias + ", ";
-                    embed.AddField(aliases.Remove(aliases.Length - 2), command.Summary);
+                    embed.AddField(string.Join(", ", command.Aliases), command.Summary);
                 }
-
-                disabled = false;
             }
 
-            // await Context.User.SendMessageAsync(embed: embed.Build());
+            // Note:
+            // https://discord.com/developers/docs/resources/channel#embed-limits
+            // title        256 characters
+            // description  4096 characters
+            // fields       Up to 25 field objects
+            // field.name   256 characters
+            // field.value  1024 characters
+            // footer.text  2048 characters
+            // author.name  256 characters
+
+            // For now I will not handle the above, but I might return to it later
+
             await ReplyAsync(embed: embed.Build());
         }
 
